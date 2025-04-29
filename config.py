7fa37f794d2ce745e04cf81d04322518f0ebd9de
TEJASVI <1bi23ee060@bit-bangalore.edu.in>
2025-04-30 00:37:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ NETWORK_PASSPHRASE = "Test SDF Network ; September 2015"
 # Replace these with your actual keys
 SOURCE_SECRET = "YOUR_TESTNET_SECRET_KEY"  # Replace with your actual secret key
 MONITOR_ACCOUNT_ID = "YOUR_PUBLIC_KEY"     # Replace with your actual public key
-DESTINATION_ACCOUNT_ID = "DESTINATION_PUBLIC_KEY"  # Optional default destination
+DESTINATION_ACCOUNT_ID = ""  # Optional default destination
